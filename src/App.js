809629d869f188bdb2804a5d8732a9a00c47516c
Kalanithi Sethuraman <kalanithi_sas@outlook.com>
2021-09-18 20:24:29 +0530

    simple count down app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import { useEffect, useState } from "react";
 import './App.css';
 
 function App() {
+
+  const [timer, setTimer] = useState(0);
+
+  useEffect(() => {
+    if(timer < 10){
+      setTimeout(() => {
+        setTimer(timer + 1);
+      }, 1000)
+    }
+  }, [timer])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Count Down Timer</h1>
+      <div className={timer < 10 ? 'timer' : 'timeOut'}>
+        <h4>{timer}</h4>
+      </div>
     </div>
   );
 }
